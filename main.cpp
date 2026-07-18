c82b5c244b02540678ad7208552ea58822685f95
marufshehzad <marufshehzad5.00@gmail.com>
2026-07-18 15:55:51 +0600

    Update student information in main.cpp

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ int main() {
     // Student Information
     // ==========================
 
-    string name = "Your Name";
-    string id = "24-XXXXX-X";
-    string section = "A";
+    string name = "Abul Bashar Maruf";
+    string id = "23-52585-2";
+    string section = "M";
 
     // ==========================
     // Personal Information
